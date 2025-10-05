9340aa1e03803cd8d6ab98fe7c17d82ac4e57eb0
Igor <ihor.kovalenko.01@gmail.com>
2025-10-05 19:53:42 +0200

    Finalize main.js according to mentor feedback

--- src/main.js
+++ src/main.js
@@ -25,15 +25,23 @@ const form = document.querySelector('.form');
 let page = 1;
 let totalPage = 1;
 
-form.addEventListener('submit', showGallery);
-loadBtn.addEventListener('click', clickLoad);
 
-async function showGallery(event) {
+hideLoadMoreButton();
+
+form.addEventListener('submit', onSearchSubmit);
+loadBtn.addEventListener('click', onLoadMore);
+
+async function onSearchSubmit(event) {
   event.preventDefault();
+
+
+  hideLoadMoreButton();
   clearGallery();
+  removeQueryLocal();
   page = 1;
 
   const query = event.target.elements['search-text'].value.trim();
+
   if (!query) {
     iziToast.error(fillText());
     return;
@@ -44,46 +52,47 @@ async function showGallery(event) {
 
     const { hits, totalHits } = await getImagesByQuery(query, page);
 
+    
     if (!hits.length) {
       iziToast.info(infoText());
+      hideLoadMoreButton();
       return;
     }
 
     createGallery(hits);
-    // scrollPage();
-
     totalPage = Math.ceil(totalHits / 15);
 
+    
     if (page < totalPage) {
-      saveQuery(query);
       showLoadMoreButton();
+      saveQuery(query);
     } else {
       hideLoadMoreButton();
-      form.reset();
     }
   } catch (error) {
     iziToast.error(errorText(error));
   } finally {
     hideLoader();
-    // form.reset();
   }
 }
 
-async function clickLoad(event) {
+async function onLoadMore() {
   page += 1;
   const query = getQuery();
 
   try {
     showLoader();
     disableButton();
+
     const { hits } = await getImagesByQuery(query, page);
     createGallery(hits);
-
     scrollPage();
+
+   
     if (page >= totalPage) {
       hideLoadMoreButton();
       removeQueryLocal();
-      iziToast.error(endText());
+      iziToast.info(endText());
       form.reset();
     }
   } catch (error) {
